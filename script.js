1dece56da2be093b6c636d94c2df14930eee3ac4
gness1804 <graham.nessler@gmail.com>
2016-08-25 14:10:05 -0600

    Centered articles

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+  $(document).on("keydown", function(key) {
+	  if (key.which === 66) {
+			$(".testing").toggleClass("border-test");
+	  }
+	  });
+}); //end of jQuery body
+
+
+
 function searchPage() {
 	var x = document.getElementById("searchbox1").value;
 	window.find(x);
